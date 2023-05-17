04eb36e32d22542a90634223cf7526ade93d7720
Muhammad Aftab Hameed <muhammad.hameed@glaston.net>
2023-05-17 21:39:43 +0300

    Port set to 5000.

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,6 @@ app.get('/*', function(req, res) {
 });
 
 // Start the server
-const PORT = 80;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
